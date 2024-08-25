d577bad26aae6147d7fa74af74193cdf4c8a6ea1
Bhargava K K <bhargavakk13@gmail.com>
2024-08-25 22:01:07 +0530

    Basic flask implemented

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,8 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
+from flask import Flask
+from flask import json
 
 
 class Blockchain:
@@ -95,3 +97,32 @@ class Blockchain:
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+    
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_id = str(uuid4()).replace('-','')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
